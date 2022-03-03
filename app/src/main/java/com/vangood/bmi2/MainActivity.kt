f3bd616e370dd4b4087a6a522e028115e3f6dcbd
Class <a90023y@gmail.com>
2022-03-03 16:38:58 +0800

    binding commid

--- app/src/main/java/com/vangood/bmi2/MainActivity.kt
+++ app/src/main/java/com/vangood/bmi2/MainActivity.kt
@@ -2,10 +2,21 @@ package com.vangood.bmi2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.vangood.bmi2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun bmi(view: View){
+        println("test123")
+        var weight = binding.edWeight.text.toString().toFloat()
+        var height = binding.edHeight.text.toString().toFloat()
     }
 }
