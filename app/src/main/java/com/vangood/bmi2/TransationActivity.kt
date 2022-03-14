dc2ac4521f545f2661ac53b16005315335d2d165
Class <a90023y@gmail.com>
2022-03-14 14:52:20 +0800

    recycler wrong

--- app/src/main/java/com/vangood/bmi2/TransationActivity.kt
+++ app/src/main/java/com/vangood/bmi2/TransationActivity.kt
@@ -2,16 +2,23 @@ package com.vangood.bmi2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
-import org.json.JSONArray
-import org.json.JSONObject
+import com.vangood.bmi2.databinding.ActivityTransationBinding
+import com.vangood.bmi2.databinding.RowRecycleviewBinding
 import java.net.URL
 import kotlin.concurrent.thread
 
 class TransationActivity : AppCompatActivity() {
+    lateinit var binding :ActivityTransationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityTransationBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_transation)
+        setContentView(binding.root)
+        binding.recycler.hasFixedSize()
+        binding.recycler.layoutManger = LinearLayoutManager(this)
         thread {
             val json = URL("https://atm201605.appspot.com/h").readText()
             val gson = Gson()
@@ -19,6 +26,8 @@ class TransationActivity : AppCompatActivity() {
             transactions.forEach {
                 println(it) //+data class
             }
+
+            //runOnUiThread { Toast.makeText(this,"Testing",Toast.LENGTH_LONG).show() }
 
             /*val transactions = mutableListOf<Transation>()
             val array = JSONArray(json)
@@ -32,7 +41,41 @@ class TransationActivity : AppCompatActivity() {
                 val tran = Transation(account,date,amount,type)
                 transactions.add(tran)
             }*/
+            runOnUiThread{
+                binding.recycler.adapter = object : RecyclerView.Adapter<TranViewHolder>() {
+                    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TranViewHolder {
+                        val binding = RowRecycleviewBinding.inflate(layoutInflater,parent,false)
+                        return TranViewHolder(binding)
+                    }
+
+                    override fun onBindViewHolder(holder: TranViewHolder, position: Int) {
+                        val tran = transactions.get(position)
+                        holder.account.setText(tran.account.toString())
+                        holder.date.setText(tran.date.toString())
+                    }
+
+                    override fun getItemCount(): Int {
+                        return transactions.size
+                    }
+
+
+                }
+            }
+
+
         }
+
+    }
+
+
+    inner class TranViewHolder(val binding: RowRecycleviewBinding):
+        RecyclerView.ViewHolder(binding.root){
+
+        val account = binding.rAccount
+        val date = binding.rDate
+        val amount = binding.rAmount
+        val type = binding.rType
+
     }
 }
 data class Transation(val account:String,val date:String,val  amount:Int,var type:Int){
